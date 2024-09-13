2845a02e21d4b2a69484d435d87b178c0642f573
parkie0517 <parkie0517@gmail.com>
2024-09-13 16:12:56 +0900

    Editted eval_lyft_rgb.sh file

--- eval_lyft_rgb.sh
+++ eval_lyft_rgb.sh
@@ -6,26 +6,36 @@ DATA_DIR="/mnt/ssd2/heejun/dataset/lyft"
 
 MODEL_NAME="16x3_5e-4s_rgb06_01:34:39"
 
-EXP_NAME="eval" # eval 16x3_5e-4s_rgb06_01:34:39 lyft
-EXP_NAME="eval" # 416, B16
-EXP_NAME="eval" # 1920,1080 base resolution
-EXP_NAME="eval" # move centroid down 1 < 24.03662218212872
-EXP_NAME="eval" # 1024 instead of 1080 (?)
+EXP_NAME="eval"
+
+
+:<<"End1"
+
+# eval 16x3_5e-4s_rgb06_01:34:39 lyft
+# 416, B16
+# 1920,1080 base resolution
+# move centroid down 1 < 24.03662218212872
+# 1024 instead of 1080 (?)
+
+End1
+
+
 EXP_NAME="eval"
 
 python eval_lyft.py \
        --exp_name=${EXP_NAME} \
-       --batch_size=2 \
+       --batch_size=1 \
        --dset='lyft' \
        --data_dir=$DATA_DIR \
        --log_dir='logs_eval_lyft' \
        --init_dir="checkpoints/${MODEL_NAME}" \
-       --res_scale=2 \
+       --res_scale=1 \
        --device_ids=[3]
 
-:<<"End"
+
+:<<"End2"
 
 Explanation of the flags
        dset: make sure is `lyft`
 
-End
+End2
